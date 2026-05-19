dc00b05a799c4e5a8cd16f285a7c2902618bb0c3
Julio Figueiredo <julioo.1amorim@gmail.com>
2026-05-19 09:27:56 -0300

    :sparkles: feat: create Celery app instance

---
+++ app/workers/celery_app.py
@@ -0,0 +1,16 @@
+from celery import Celery
+from app.core.config import settings
+
+celery = Celery(
+    "async_report",
+    broker=settings.redis_url,
+    backend=settings.redis_url,
+)
+
+celery.conf.update(
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    result_expires=3600,
+    task_track_started=True,
+)
